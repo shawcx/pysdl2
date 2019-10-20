a877646769674f25b4dd18b876e1531b1bcbb33b
Matthew Oertle <moertle@gmail.com>
2019-10-20 14:44:34 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ source_files = glob.glob('./src/*.c')
 
 setuptools.setup(
     name             = 'SDL2',
+    version          = '0.2',
     author           = 'Matthew Oertle',
     author_email     = 'moertle@gmail.com',
-    version          = '0.2',
-    license          = 'MIT',
     url              = 'https://github.com/moertle/pysdl2',
+    license          = 'MIT',
     description      = 'Python3 bindings for SDL2',
     long_description = open('README.rst').read(),
     ext_modules = [
