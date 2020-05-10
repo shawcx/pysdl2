5213b31e0558309d3a71f5fa5b3f52fad672176f
Matthew Oertle <moertle@gmail.com>
2020-05-10 15:49:04 -0400

    multiple screen support

--- test/adjust.py
+++ test/adjust.py
@@ -7,27 +7,26 @@ import SDL2
 # Initialize SDL2
 SDL2.Init(SDL2.INIT_VIDEO)
 
-# Get the current resolution
-params = SDL2.GetDisplayMode(0)
-w = params[1]
-h = params[2]
+numberOfDisplays = SDL2.GetNumVideoDisplays()
 
-# Create the window
-window = SDL2.Window("Adjust", size=(w,h), flags=SDL2.WINDOW_FULLSCREEN)
+windows = []
+for i in range(numberOfDisplays):
+    x,y,w,h = SDL2.GetDisplayBounds(i)
+    window = SDL2.Window("Adjust", size=(w,h), position=(x,y), flags=SDL2.WINDOW_FULLSCREEN)
+    windows.append((window, w, h))
 
 # Hide the cursor
 SDL2.ShowCursor(SDL2.DISABLE)
 
-surface = window.GetWindowSurface()
-
 white = b'\xff\xff\xff\x00'
 black = b'\x00\x00\x00\x00'
 
-# blit alternating black and white pixels to the screen
-surface.LockSurface()
-surface.pixels = ((white + black) * int(w/2) + (black + white) * int(w/2)) * int(h/2)
-surface.UnlockSurface()
-window.UpdateWindowSurface()
+for window,w,h in windows:
+    surface = window.GetWindowSurface()
+    surface.LockSurface()
+    surface.pixels = ((white + black) * int(w/2) + (black + white) * int(w/2)) * int(h/2)
+    surface.UnlockSurface()
+    window.UpdateWindowSurface()
 
 while True:
     event = SDL2.WaitEvent()
@@ -39,5 +38,4 @@ while True:
 
 # Show the cursor
 SDL2.ShowCursor(SDL2.ENABLE)
-
 SDL2.Quit()
